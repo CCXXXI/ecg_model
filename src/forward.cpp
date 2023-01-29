41cee1e6592f658ec48dbc9f0ffdc821510b68ea
CCXXXI <ccxxxi@qq.com>
2023-01-29 17:52:23 +0800

    style: const

--- src/forward.cpp
+++ src/forward.cpp
@@ -2,20 +2,21 @@
 
 torch::jit::Module model;
 
-extern "C" auto load_model(const char* path) -> void {
+extern "C" auto load_model(const char* const path) -> void {
   model = torch::jit::load(path);
   model.eval();
 }
 
-auto forward(double* data, int size) -> at::Tensor {
+auto forward(double* const data, const int size) -> at::Tensor {
   std::vector<torch::jit::IValue> inputs;
   inputs.emplace_back(torch::from_blob(data, {1, 1, size}, torch::kDouble));
 
-  torch::NoGradGuard no_grad;
+  const torch::NoGradGuard no_grad;
   return model.forward(inputs).toTensor()[0];
 }
 
-extern "C" auto forward_argmax(double* data, int size, uint8_t* out) -> void {
-  auto res = forward(data, size).argmax(0).to(torch::kU8);
+extern "C" auto forward_argmax(double* const data, const int size,
+                               uint8_t* const out) -> void {
+  const auto res = forward(data, size).argmax(0).to(torch::kU8);
   memcpy(out, res.data_ptr(), size);
 }
--- tests/test_forward.cpp
+++ tests/test_forward.cpp
@@ -19,21 +19,21 @@ TEST_CASE("test_forward", "[ecg_model]") {
   auto input = load_txt<double>("resources/input.txt");
   REQUIRE(input.size() == n);
 
-  auto direct_output = forward(input.data(), n);
+  const auto direct_output = forward(input.data(), n);
   REQUIRE(direct_output.sizes() == std::vector<int64_t>{m, n});
 
   auto expected_direct_output_raw =
       load_txt<double>("resources/direct_output.txt");
-  auto expected_direct_output = torch::from_blob(
+  const auto expected_direct_output = torch::from_blob(
       expected_direct_output_raw.data(), {m, n}, torch::kDouble);
   REQUIRE(direct_output.allclose(expected_direct_output));
 
-  auto argmax_output = direct_output.argmax(0);
+  const auto argmax_output = direct_output.argmax(0);
   REQUIRE(argmax_output.sizes() == std::vector<int64_t>{n});
 
   auto expected_argmax_output_raw =
       load_txt<int64_t>("resources/argmax_output.txt");
-  auto expected_argmax_output =
+  const auto expected_argmax_output =
       torch::from_blob(expected_argmax_output_raw.data(), {n}, torch::kInt64);
   REQUIRE(argmax_output.equal(expected_argmax_output));
 }
