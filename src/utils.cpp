4e04971e81e1d9f410eb599ca62ff68c9aa87b80
CCXXXI <ccxxxi@qq.com>
2023-04-30 17:29:11 +0800

    refactor: implement utils

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,15 @@
 #include "utils.h"
+
+#include <torch/script.h>
+
+namespace {
+std::string models_path;
+}
+
+auto set_models_path(const std::string& path) -> void { models_path = path; }
+
+auto load_model(const std::string& filename) -> torch::jit::Module {
+  auto model = torch::jit::load(models_path + filename);
+  model.eval();
+  return model;
+}
