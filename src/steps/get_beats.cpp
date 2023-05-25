9e23187faf9413c03c8295ef86479c05122dcdec
CCXXXI <ccxxxi@qq.com>
2023-05-25 09:12:37 +0800

    refactor(get-beats): add const

--- src/steps/get_beats.cpp
+++ src/steps/get_beats.cpp
@@ -1,6 +1,6 @@
 #include "get_beats.h"
 
-auto get_beats(const nc::NdArray<double>& data, int ori_fs)
+auto get_beats(const nc::NdArray<double>& data, const int ori_fs)
     -> std::vector<Beat> {
   // todo: implement
 }
