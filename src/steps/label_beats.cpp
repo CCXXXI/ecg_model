ca8534d62230a05c36220371b617ec54bb22e4a5
CCXXXI <ccxxxi@qq.com>
2023-05-25 09:48:36 +0800

    refactor(label-beats): add bsw

--- src/steps/label_beats.cpp
+++ src/steps/label_beats.cpp
@@ -1,5 +1,11 @@
 #include "label_beats.h"
 
+namespace {
+auto bsw(const nc::NdArray<double>& data) -> nc::NdArray<double> {
+  // todo: implement
+}
+}  // namespace
+
 auto label_beats(const nc::NdArray<double>& data,
                  const std::vector<Beat>& beats, const int ori_fs)
     -> std::vector<Beat> {
