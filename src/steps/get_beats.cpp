4d6533d34461203a89952437a814522778397cbc
CCXXXI <ccxxxi@qq.com>
2023-05-25 09:56:45 +0800

    refactor(get-beats): reorder functions

--- src/steps/get_beats.cpp
+++ src/steps/get_beats.cpp
@@ -1,16 +1,16 @@
 #include "get_beats.h"
 
 namespace {
+auto output_sliding_voting_v2(const nc::NdArray<int>& ori_output)
+-> nc::NdArray<int> {
+  // todo: implement
+}
+
 auto u_net_peak(const nc::NdArray<double>& data) -> nc::NdArray<bool> {
   // todo: implement
 }
 
 auto u_net_r_peak(const nc::NdArray<bool>& is_qrs) -> std::vector<int> {
-  // todo: implement
-}
-
-auto output_sliding_voting_v2(const nc::NdArray<int>& ori_output)
-    -> nc::NdArray<int> {
   // todo: implement
 }
 }  // namespace
