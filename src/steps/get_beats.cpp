6f91a8db01e77ada1770c942865de20e0174f1d6
CCXXXI <ccxxxi@qq.com>
2023-05-25 10:24:50 +0800

    refactor(get-beats): implement output_sliding_voting_v2

--- src/steps/get_beats.cpp
+++ src/steps/get_beats.cpp
@@ -2,8 +2,28 @@
 
 namespace {
 auto output_sliding_voting_v2(const nc::NdArray<int>& ori_output)
--> nc::NdArray<int> {
-  // todo: implement
+    -> nc::NdArray<int> {
+  static constexpr auto window = 9;
+
+  auto output = ori_output;
+  const auto n = static_cast<int>(output.size());
+  const auto half_window = window / 2;
+  auto cnt = nc::zeros<int>(4, 1);
+  auto l_index = 0;
+  auto r_index = -1;
+  for (auto i = 0; i < n; ++i) {
+    if (r_index - l_index + 1 == window && half_window < i &&
+        i < n - half_window) {
+      --cnt[ori_output[l_index]];
+      ++l_index;
+    }
+    while (r_index - l_index + 1 < window && r_index + 1 < n) {
+      ++r_index;
+      ++cnt[ori_output[r_index]];
+    }
+    output[i] = static_cast<int>(nc::argmax(cnt).front());
+  }
+  return output;
 }
 
 auto u_net_peak(const nc::NdArray<double>& data) -> nc::NdArray<bool> {
