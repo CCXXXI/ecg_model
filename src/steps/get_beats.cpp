d023f29f809cb533b3654d0fa9633e2be6f6298d
CCXXXI <ccxxxi@qq.com>
2023-05-25 09:27:18 +0800

    refactor(get-beats): add u_net_peak

--- src/steps/get_beats.cpp
+++ src/steps/get_beats.cpp
@@ -1,5 +1,11 @@
 #include "get_beats.h"
 
+namespace {
+auto u_net_peak(const nc::NdArray<double>& data) -> nc::NdArray<bool> {
+  // todo: implement
+}
+}  // namespace
+
 auto get_beats(const nc::NdArray<double>& data, const int ori_fs)
     -> std::vector<Beat> {
   // todo: implement
