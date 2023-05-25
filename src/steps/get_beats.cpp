71693080f844b050fea5c5570d6c180795c4c9a0
CCXXXI <ccxxxi@qq.com>
2023-05-25 09:35:42 +0800

    refactor(get-beats): add output_sliding_voting_v2

--- src/steps/get_beats.cpp
+++ src/steps/get_beats.cpp
@@ -8,6 +8,11 @@ auto u_net_peak(const nc::NdArray<double>& data) -> nc::NdArray<bool> {
 auto u_net_r_peak(const nc::NdArray<bool>& is_qrs) -> std::vector<int> {
   // todo: implement
 }
+
+auto output_sliding_voting_v2(const nc::NdArray<int>& ori_output)
+    -> nc::NdArray<int> {
+  // todo: implement
+}
 }  // namespace
 
 auto get_beats(const nc::NdArray<double>& data, const int ori_fs)
