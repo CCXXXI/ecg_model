02c75fff401e1410236287f7b467f2f3eab99ac1
CCXXXI <ccxxxi@qq.com>
2023-06-04 20:08:14 +0800

    refactor(label-beats): implement transform

--- src/steps/label_beats.cpp
+++ src/steps/label_beats.cpp
@@ -2,9 +2,17 @@
 
 #include <torch/all.h>
 
+#include "../scipy/scipy.h"
+
 namespace {
 auto transform(const nc::NdArray<double>& sig) -> torch::Tensor {
-  // todo: implement
+  auto sig_vector = sig.toStlVector();
+  const auto n = static_cast<int>(sig_vector.size());
+  auto sig_resampled_vector = std::vector<double>{};
+  scipy::resample<double>(360, n, sig_vector, sig_resampled_vector);
+  auto sig_resampled = nc::NdArray<double>(sig_resampled_vector);
+  auto sig_tensor = torch::from_blob(sig_resampled.data(), {1, 360});
+  return sig_tensor;
 }
 }  // namespace
 
