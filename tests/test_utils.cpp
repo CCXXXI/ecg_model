599da281cf16e8f0686dca7657c6d234db7cc562
Restyled.io <commits@restyled.io>
2023-06-04 12:55:35 +0000

    style: restyle by clang-format

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -4,10 +4,10 @@
 #include "../src/utils.h"
 
 TEST_CASE("test_load_model", "[utils]") {
-    set_models_path("./assets/ecg_models/models/");
+  set_models_path("./assets/ecg_models/models/");
 
-    auto filename = GENERATE("u_net.pt", "res_net.pt");
-    auto model = load_model(filename);
+  auto filename = GENERATE("u_net.pt", "res_net.pt");
+  auto model = load_model(filename);
 
-    REQUIRE(!model.is_training());
+  REQUIRE(!model.is_training());
 }
