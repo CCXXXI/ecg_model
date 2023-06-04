c5bbb11be9f62bb501581499e1aef62452532579
CCXXXI <ccxxxi@qq.com>
2023-06-04 20:17:53 +0800

    refactor(resample): inline quotientCeil
    resolve compilation error

--- src/scipy/resample.h
+++ src/scipy/resample.h
@@ -29,7 +29,7 @@ T sinc(T x) {
   return std::sin(PI * x) / (PI * x);
 }
 
-int quotientCeil(int num1, int num2) {
+inline int quotientCeil(int num1, int num2) {
   if (num1 % num2 != 0) return num1 / num2 + 1;
   return num1 / num2;
 }
