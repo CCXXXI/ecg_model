c9d10b1f103a3e95d9d62be2ea66d07101f5de48
CCXXXI <ccxxxi@qq.com>
2023-06-06 12:53:15 +0800

    refactor: add main.cpp/.h

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ccxxxi on 6/6/23.
+//
+
+#include "main.h"
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by ccxxxi on 6/6/23.
+//
+
+#ifndef ECG_MODEL_MAIN_H
+#define ECG_MODEL_MAIN_H
+
+#endif  // ECG_MODEL_MAIN_H
