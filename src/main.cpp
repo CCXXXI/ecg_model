cc3896623a1755c39b173b4aceb43dd246670ffc
CCXXXI <ccxxxi@qq.com>
2023-06-06 13:58:28 +0800

    refactor(main): add const

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,9 @@ auto infer(const nc::NdArray<double>& data, const int ori_fs)
 
 auto get_input() -> nc::NdArray<double> {
   std::ifstream in("./assets/ecg_data/assets/data.json");
-  auto points = nlohmann::json::parse(in);
+  const auto points = nlohmann::json::parse(in);
 
-  auto n = static_cast<int>(points.size());
+  const auto n = static_cast<int>(points.size());
   auto data = nc::NdArray<double>(1, n);
   for (auto i = 0; i < n; ++i) {
     data(0, i) = points[i]["leadII"];
